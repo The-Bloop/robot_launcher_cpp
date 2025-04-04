2e7d17b2f3c902dae518928588bf6e31b6c8271f
The-Bloop <ssabariis@gmail.com>
2025-04-04 14:17:27 -0600

    1. Some changes to RViz2 config file.

--- launch/rlcpp_gazebo.launch.py
+++ launch/rlcpp_gazebo.launch.py
@@ -93,11 +93,11 @@ def generate_launch_description():
         )
     )
 
-    #robot_localization_cmd = IncludeLaunchDescription(
-    #    PythonLaunchDescriptionSource(
-    #        os.path.join(pkg_share, 'launch', 'robot_localization.launch.py')
-    #    ),
-    #)
+    robot_localization_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_share, 'launch', 'robot_localization.launch.py')
+        ),
+    )
 
     #Creating Nodes
     #To control log level of nodes add the following to the node's Arguments:
@@ -162,7 +162,7 @@ def generate_launch_description():
     #ld.add_action(ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'], output='screen'))
     ld.add_action(start_robot_state_publisher_cmd)
     ld.add_action(start_gazebo_ros_spawner_cmd)
-    #ld.add_action(robot_localization_cmd)
+    ld.add_action(robot_localization_cmd)
     ld.add_action(start_rviz_cmd)
     
 
