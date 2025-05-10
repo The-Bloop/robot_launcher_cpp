e88b786cc1eb84de0bc05ce222b2ba1d4635744f
The-Bloop <ssabariis@gmail.com>
2025-05-09 20:46:46 -0600

    It's ALIVE! Make sure not to repeat gazebo plugins in both the urdf and the sdf files.

--- launch/rlcpp_gazebo.launch.py
+++ launch/rlcpp_gazebo.launch.py
@@ -1,10 +1,10 @@
 import os
-import xacro
+# import xacro
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, ExecuteProcess
 from launch.conditions import IfCondition
-from launch.substitutions import Command, LaunchConfiguration, FindExecutable
+from launch.substitutions import Command, LaunchConfiguration, FindExecutable, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -13,11 +13,11 @@ def generate_launch_description():
 
     #File Paths
     pkg_share = FindPackageShare(package='robot_launcher_cpp').find('robot_launcher_cpp')
-    urdf_model_path = os.path.join(pkg_share, 'urdf/wrv4/wrv4.xacro')
+    urdf_model_path = os.path.join(pkg_share, 'urdf/wrv4/wrv4.urdf.xacro')
     sdf_model_path = os.path.join(pkg_share, 'models', 'wrv4', 'model.sdf')
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/rviz_basic_settings.rviz')
-    robot_description_raw = xacro.process_file(urdf_model_path).toxml()
+    # robot_description_raw = xacro.process_file(urdf_model_path).toxml()
     world = os.path.join(
         pkg_share,
         'worlds',
@@ -38,62 +38,66 @@ def generate_launch_description():
 
     #Declaring Launch Arguments
 
-    declare_use_rviz_cmd = DeclareLaunchArgument(
+    declare_use_rviz_arg = DeclareLaunchArgument(
         name='use_rviz',
         default_value='True',
         description='Flag to control use of RVIZ'
     )
 
-    declare_logger_cmd = DeclareLaunchArgument(
+    declare_logger_arg = DeclareLaunchArgument(
         name='log_level',
         default_value="info",
         description='Indicates logging level of Nodes'
     )
         
-    declare_gazebo_log_cmd = DeclareLaunchArgument(
+    declare_gazebo_log_arg = DeclareLaunchArgument(
         name='gazebo_log',
         default_value="false",
         description='Indicates gazebo verbosity'
     )
 
-    declare_sim_time_cmd = DeclareLaunchArgument(
+    declare_sim_time_arg = DeclareLaunchArgument(
         name='use_sim_time',
         default_value="true",
         description='On/Off Simulation time'
     )
 
-    declare_x_pose_cmd = DeclareLaunchArgument(
+    declare_x_pose_arg = DeclareLaunchArgument(
         name='x_pose',
         default_value=x_pose,
         description="Position of robot along X axis"
     )
 
-    declare_y_pose_cmd = DeclareLaunchArgument(
+    declare_y_pose_arg = DeclareLaunchArgument(
         name='y_pose',
         default_value=y_pose,
         description="Position of robot along Y axis"
     )
 
-    robot_description = Command([FindExecutable(name='xacro'), ' ', urdf_model_path])
+    robot_description = Command([FindExecutable(name='xacro'), ' ',
+                                 PathJoinSubstitution([
+                                     FindPackageShare('robot_launcher_cpp'), 
+                                     'urdf/wrv4', 'wrv4.urdf.xacro'])
+                                     ])
 
     #Initializing Other Launch Files
     #To change log level of the launch files add the following in launch_arguments:
     #'verbose': gazebo_log
 
-    gzserver_cmd = IncludeLaunchDescription(
+    gzserver_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
         ),
         launch_arguments={'world': world,'verbose': gazebo_log}.items()
     )
 
-    gzclient_cmd = IncludeLaunchDescription(
+    gzclient_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
         )
     )
 
-    robot_localization_cmd = IncludeLaunchDescription(
+    robot_localization_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_share, 'launch', 'robot_localization.launch.py')
         ),
@@ -103,7 +107,7 @@ def generate_launch_description():
     #To control log level of nodes add the following to the node's Arguments:
     # '--ros-args','--log-level',logger
 
-    start_joint_state_publisher_cmd = Node(
+    joint_state_publisher_node = Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher',
@@ -111,7 +115,7 @@ def generate_launch_description():
         parameters=[{'use_sim_time': use_sim_time}],
         )
 
-    start_robot_state_publisher_cmd = Node(
+    robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
@@ -121,12 +125,12 @@ def generate_launch_description():
         arguments=['--ros-args','--log-level',logger]
     )
 
-    start_gazebo_ros_spawner_cmd = Node(
+    gazebo_ros_spawner_node = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
         arguments=[
+            '-topic', 'robot_description',
             '-entity', 'WheelRobotv4',
-            '-file', sdf_model_path,
             '-x', x_pose,
             '-y', y_pose,
             '-z', '0.01',
@@ -134,8 +138,7 @@ def generate_launch_description():
         ],
         output='screen',
     )
-
-    start_rviz_cmd = Node(
+    rviz_node = Node(
         condition=IfCondition(use_rviz),
         package='rviz2',
         executable='rviz2',
@@ -148,22 +151,22 @@ def generate_launch_description():
 
     # Add the commands to the launch description
     #Launch Arguments
-    ld.add_action(declare_use_rviz_cmd)
-    ld.add_action(declare_logger_cmd)
-    ld.add_action(declare_gazebo_log_cmd)
-    ld.add_action(declare_sim_time_cmd)
-    ld.add_action(declare_x_pose_cmd)
-    ld.add_action(declare_y_pose_cmd)
+    ld.add_action(declare_use_rviz_arg)
+    ld.add_action(declare_logger_arg)
+    ld.add_action(declare_gazebo_log_arg)
+    ld.add_action(declare_sim_time_arg)
+    ld.add_action(declare_x_pose_arg)
+    ld.add_action(declare_y_pose_arg)
 
     #Launch Files
-    ld.add_action(gzserver_cmd)
-    ld.add_action(gzclient_cmd)
-    ld.add_action(start_joint_state_publisher_cmd)
+    ld.add_action(gzserver_launch)
+    ld.add_action(gzclient_launch)
+    ld.add_action(joint_state_publisher_node)
     #ld.add_action(ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'], output='screen'))
-    ld.add_action(start_robot_state_publisher_cmd)
-    ld.add_action(start_gazebo_ros_spawner_cmd)
-    ld.add_action(robot_localization_cmd)
-    ld.add_action(start_rviz_cmd)
+    ld.add_action(robot_state_publisher_node)
+    ld.add_action(gazebo_ros_spawner_node)
+    ld.add_action(robot_localization_launch)
+    ld.add_action(rviz_node)
     
 
     return ld
